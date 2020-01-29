9093edc6252b199a4ee37bf9319f835e88d7a934
HUEN_OH <heons921@gmail.com>
2020-01-28 21:41:46 -0500

    update : splash screen

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -60,7 +60,7 @@ export class AppComponent {
   initializeApp() {
     this.platform.ready().then(() => {
       this.statusBar.styleDefault();
-      this.splashScreen.hide();
+      // this.splashScreen.hide();
     });
   }
 }

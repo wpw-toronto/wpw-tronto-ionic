ea22e045ccc1455e6b4baa691f5757338b3fc62a
HUEN_OH <heons921@gmail.com>
2020-03-21 12:32:03 -0400

    fix : test error in login  page

--- src/app/loginpage/loginpage.page.spec.ts
+++ src/app/loginpage/loginpage.page.spec.ts
@@ -3,6 +3,23 @@ import { IonicModule } from '@ionic/angular';
 
 import { LoginpagePage } from './loginpage.page';
 
+import { RouterTestingModule } from '@angular/router/testing';
+import { AngularFireModule } from '@angular/fire';
+import { AngularFireAuthModule, AngularFireAuth } from '@angular/fire/auth';
+import { FirebaseUIModule, firebase, firebaseui } from 'firebaseui-angular';
+import { environment } from '../../environments/environment';
+
+const firebaseUiAuthConfig: firebaseui.auth.Config = {
+  signInFlow: 'popup',
+  signInOptions: [
+    firebase.auth.EmailAuthProvider.PROVIDER_ID,
+  ],
+  tosUrl: '<your-tos-link>',
+  privacyPolicyUrl: '<your-privacyPolicyUrl-link>',
+  credentialHelper: firebaseui.auth.CredentialHelper.NONE
+};
+
+
 describe('LoginpagePage', () => {
   let component: LoginpagePage;
   let fixture: ComponentFixture<LoginpagePage>;
@@ -10,7 +27,16 @@ describe('LoginpagePage', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [ LoginpagePage ],
-      imports: [IonicModule.forRoot()]
+      providers: [
+        AngularFireAuth,
+      ],
+      imports: [
+        IonicModule.forRoot(),
+        RouterTestingModule.withRoutes([]),
+        AngularFireModule.initializeApp(environment.firebase),
+        AngularFireAuthModule,
+        FirebaseUIModule.forRoot(firebaseUiAuthConfig)
+      ]
     }).compileComponents();
 
     fixture = TestBed.createComponent(LoginpagePage);

93361d77962db52eed089c10b41c01c938a25f3e
HUEN_OH <heons921@gmail.com>
2020-03-21 12:17:52 -0400

    fix : test error in signout page

--- src/app/signout/signout.page.spec.ts
+++ src/app/signout/signout.page.spec.ts
@@ -3,6 +3,11 @@ import { IonicModule } from '@ionic/angular';
 
 import { SignoutPage } from './signout.page';
 
+import { RouterTestingModule } from '@angular/router/testing';
+import { AngularFireModule } from '@angular/fire';
+import { AngularFireAuthModule, AngularFireAuth } from '@angular/fire/auth';
+import { environment } from '../../environments/environment';
+
 describe('SignoutPage', () => {
   let component: SignoutPage;
   let fixture: ComponentFixture<SignoutPage>;
@@ -10,7 +15,15 @@ describe('SignoutPage', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [ SignoutPage ],
-      imports: [IonicModule.forRoot()]
+      providers: [
+        AngularFireAuth,
+      ],
+      imports: [
+        IonicModule.forRoot(),
+        RouterTestingModule.withRoutes([]),
+        AngularFireModule.initializeApp(environment.firebase), //ajout
+        AngularFireAuthModule //ajout
+      ]
     }).compileComponents();
 
     fixture = TestBed.createComponent(SignoutPage);

392c64ddd2220fe4e18a2a11fdf0d195cf2b7f37
Ignat Pechkurenko <pechkig@gmail.com>
2020-03-29 23:10:15 -0400

    A few images are downloaded from firebase storage.

--- src/app/foodtruck/foodtruck.page.ts
+++ src/app/foodtruck/foodtruck.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import * as firebase from 'firebase/app';
+import 'firebase/storage';
+
 
 @Component({
   selector: 'app-foodtruck',
@@ -10,6 +13,50 @@ export class FoodtruckPage implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    // Points to the root reference
+    var storageRef = firebase.storage().ref();
+    // Create a reference to the file we want to download
+    var starsRef = storageRef.child('food_truck_sample.jpg');
+
+    // Get the download URL
+    starsRef.getDownloadURL().then(function (url) {
+      // Insert url into an <img> tag to "download"
+      
+      var foodTruck1Img = document.getElementById("foodTruck1Img") as HTMLImageElement;
+      var foodTruck2Img = document.getElementById("foodTruck2Img") as HTMLImageElement;
+      var foodTruck3Img = document.getElementById("foodTruck3Img") as HTMLImageElement;
+
+      foodTruck1Img.src = url;
+      foodTruck2Img.src = url;
+      foodTruck3Img.src = url;
+
+    }).catch(function (error) {
+
+      // A full list of error codes is available at
+      // https://firebase.google.com/docs/storage/web/handle-errors
+      switch (error.code) {
+        case 'storage/object-not-found':
+          console.log("File doesn't exist");
+          // File doesn't exist
+          break;
+
+        case 'storage/unauthorized':
+          console.log("User doesn't have permission to access the object");
+          // User doesn't have permission to access the object
+          break;
+
+        case 'storage/canceled':
+          console.log("User canceled the upload");
+          // User canceled the upload
+          break;
+
+        case 'storage/unknown':
+          console.log("Unknown error occurred, inspect the server response");
+          // Unknown error occurred, inspect the server response
+          break;
+      }
+    });
   }
+
 
 }
--- src/app/map-event/map-event.page.ts
+++ src/app/map-event/map-event.page.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { ModalController } from '@ionic/angular';
+import { ViewerModalComponent} from 'ngx-ionic-image-viewer';
+import * as firebase from 'firebase/app';
+import 'firebase/storage';
+
 
 @Component({
   selector: 'app-map-event',
@@ -6,10 +11,62 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./map-event.page.scss'],
 })
 export class MapEventPage implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+  constructor(public modalController: ModalController)
+  {
   }
 
+  async openViewer() {
+    var mapEventImg = document.getElementById("mapEventImg") as HTMLImageElement;
+    var imgToShow = mapEventImg.src;
+    const modal = await this.modalController.create({
+      component: ViewerModalComponent,
+      componentProps: {
+        src: imgToShow
+      },
+      cssClass: 'ion-img-viewer',
+      keyboardClose: true,
+      showBackdrop: true
+    });
+
+    return await modal.present();
+  }
+
+  ngOnInit() {
+    // Points to the root reference
+    var storageRef = firebase.storage().ref();
+    // Create a reference to the file we want to download
+    var starsRef = storageRef.child('WPW_2020_Site GuideMap.png');
+
+    // Get the download URL
+    starsRef.getDownloadURL().then(function (url) {
+      // Insert url into an <img> tag to "download"
+      var mapEventImg = document.getElementById("mapEventImg") as HTMLImageElement;
+      mapEventImg.src = url;
+    }).catch(function (error) {
+
+      // A full list of error codes is available at
+      // https://firebase.google.com/docs/storage/web/handle-errors
+      switch (error.code) {
+        case 'storage/object-not-found':
+          console.log("File doesn't exist");
+          // File doesn't exist
+          break;
+
+        case 'storage/unauthorized':
+          console.log("User doesn't have permission to access the object");
+          // User doesn't have permission to access the object
+          break;
+
+        case 'storage/canceled':
+          console.log("User canceled the upload");
+          // User canceled the upload
+          break;
+
+        case 'storage/unknown':
+          console.log("Unknown error occurred, inspect the server response");
+          // Unknown error occurred, inspect the server response
+          break;
+      }
+    });
+  }
 }
